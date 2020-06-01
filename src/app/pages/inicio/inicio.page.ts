32dd28e118103b370205eaebf537e167bfec303d
Jose Ganchozo <joseganchozo@outlook.com>
2020-06-01 09:43:44 -0500

    action sheet

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    { icon: 'american-football', name: 'Action Sheet', redirectTo: '/action-sheet' },
+    { icon: 'logo-google-playstore', name: 'Alert', redirectTo: '/alert' }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
