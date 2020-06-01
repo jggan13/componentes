f9d7ff28c9e31dd563cb2fa4466a17578c7655e7
Jose Ganchozo <joseganchozo@outlook.com>
2020-06-01 17:25:04 -0500

    menu

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Componente } from 'src/app/interfaces/interfaces';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,32 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  componentes: Componente[] = [
-    { icon: 'american-football', name: 'Action Sheet', redirectTo: '/action-sheet' },
-    { icon: 'logo-google-playstore', name: 'Alert', redirectTo: '/alert' },
-    { icon: 'beaker', name: 'Avatar', redirectTo: '/avatar' },
-    { icon: 'radio-button-on', name: 'Botones y router', redirectTo: '/botones' },
-    { icon: 'card', name: 'Cards', redirectTo: '/card' },
-    { icon: 'checkmark-circle-outline', name: 'Checkbox', redirectTo: '/check' },
-    { icon: 'calendar', name: 'DateTime', redirectTo: '/date-time' },
-    { icon: 'car', name: 'Fabs', redirectTo: '/fab' },
-    { icon: 'grid', name: 'Grid - Rows', redirectTo: '/grid' },
-    { icon: 'infinite', name: 'Infinite Scroll', redirectTo: '/infinite-scroll' },
-    { icon: 'hammer', name: 'Input - Forms', redirectTo: '/input' },
-    { icon: 'list', name: 'Listas - Sliding', redirectTo: '/list' },
-    { icon: 'reorder-four', name: 'Listas - Reorder', redirectTo: '/list-reorder' },
-    { icon: 'refresh-circle', name: 'Loading', redirectTo: '/loading' }
-  ];
+  componentes: Observable<Componente[]>;
 
-  constructor() { }
+  constructor(private menuController: MenuController, private dataService: DataService ) { }
 
   ngOnInit() {
+    this.componentes = this.dataService.getMenu();
   }
 
-}
+  toggleMenu = () => {
+    this.menuController.toggle();
+  }
 
-interface Componente {
-  icon: string;
-  name: string;
-  redirectTo: string;
 }
